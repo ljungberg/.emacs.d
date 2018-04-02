e724ba25bc6d63b00cfc04e23fd0f345710bfbe7
Nils Ljungberg <nils.ljungberg@gmail.com>
2018-04-02 12:23:20 +0200

    first melpa try

---
+++ init.el
@@ -0,0 +1,7 @@
+;; No splash screen please ... jeez
+(setq inhibit-startup-message t)
+
+
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
